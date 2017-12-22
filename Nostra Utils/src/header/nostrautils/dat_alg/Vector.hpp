d464b62d4cb6829284297613af91da411e0baa37
Dennis Franz <DennisFranz96@gmx.net>
2017-12-22 19:17:26 +0100

    Vector implementation
    Created the Vector.hpp and made a Vector class

---
+++ Nostra Utils/src/header/nostrautils/dat_alg/Vector.hpp
@@ -0,0 +1,25 @@
+#ifndef NOU_DAT_ALG_VECTOR_HPP
+#define	NOU_DAT_ALG_VECTOR_HPP
+
+#include "nostrautils\core\StdIncludes.hpp"
+
+namespace NOU::NOU_DAT_ALG
+{
+	template<typename T>
+	class Vector
+	{
+	private:
+
+	public:
+		Vector<T>();
+		~Vector<T>();
+	};
+
+
+	template<typename T>
+	Vector<T>::Vector()
+	{}
+}
+
+
+#endif
