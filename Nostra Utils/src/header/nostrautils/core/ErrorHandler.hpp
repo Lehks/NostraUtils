5e1e201705d1e4fbc49adc83981c67dbcc4ecc02
Lukas Gross <lukasgross96@web.de>
2018-01-03 16:55:40 +0100

    Removed empty() in Error and added additional documentation.

--- Nostra Utils/src/header/nostrautils/core/ErrorHandler.hpp
+++ Nostra Utils/src/header/nostrautils/core/ErrorHandler.hpp
@@ -17,6 +17,9 @@
 namespace NOU::NOU_CORE
 {
 	/**
+	\tparam T The type of the class.
+
+	\brief Defines the ErrorHandler class.
 	*/
 	template<typename T>
 	class NOU_CLASS ErrorHandler
@@ -38,28 +41,96 @@ namespace NOU::NOU_CORE
 		T& queryError(sizeType errorCode) override;
 	};
 
+	/**
+	\tparam The error location type.
+	
+	\brief Defines an error location object with its attributes and functions.
+	*/
 	template<typename T>
 	class NOU_CLASS ErrorLocation
 	{
 	private:
-		char32 m_fnName; //Besser string? Warten auf String implementierung von Dennis.
+		/**
+		\brief The function name in which the error occured.
+		*/
+		char32 m_fnName; //Warten auf String implementierung von Dennis.
+		
+		/**
+		\brief The line in which the error occured.
+		*/
 		sizeType m_line;
+
+		/**
+		\brief The file in which the error occured.
+		*/
 		char32 m_file;
+
+		/**
+		\brief The error message which is showed.
+		*/
 		char32 m_msg;
 	public:
+
+		/**
+		\return Returns the function name.
+
+		\brief Returns the name of the function in which the error occured.
+		*/
 		char32 getFnName() override;
+
+		/**
+		\return Returns the line.
+		
+		\brief Returns the line in which the error occured.
+		*/
 		sizeType getLine() override;
+
+		/**
+		\return Returns the file.
+		
+		\brief Returns the file in which the error occured.
+		*/
 		char32 getFile() override;
+
+		/**
+		\return Returns the error message.
+		
+		\brief Returns the error message of the error.
+		*/
 		char32 getMsg() override;
 		
+		/**
+		\param fnName The name of the function.
+
+		\brief Sets an new name for the function in which the error occured. 
+		*/
 		void setFnName(char32 fnName) override;
+
+		/**
+		\param line The line of the error.
+
+		\brief Sets an new line in which the error occured.
+		*/
 		void setLine(sizeType line) override;
+		
+		/**
+		\param file The name of the file.
+
+		\brief Sets an new file in which the error occured.
+		*/
 		void setFile(char32 file) override;
-		void setMsg(char33 msg) override;
+		
+		/**
+		\param msg The new message
+
+		\brief Sets an new message for the error.
+		*/
+		void setMsg(char32 msg) override;
 	};
 
 	/**
 	\tparam The error type
+	
 	\brief Defines an error object with its attributes and functions.
 	*/
 	template<typename T>
@@ -81,12 +152,14 @@ namespace NOU::NOU_CORE
 
 		/**
 		\return Returns the name.
+		
 		\brief Returns the name of the error object.
 		*/
 		char32 getName() override;
 
 		/**
 		\return Returns the id.
+		
 		\Brief Returns the id of the error object.
 		*/
 		sizeType getID() override;
@@ -104,15 +177,6 @@ namespace NOU::NOU_CORE
 		\brief Sets a new error name. 
 		*/
 		void setName(char32 name) override;
-
-		/**
-		\param string The string which will be checked.
-
-		\return Returns a boolean value.
-
-		\brief Checks if the given string is empty.
-		*/
-		boolean empty(char32 string) override;
 	};
 
 	template<typename T>
@@ -130,21 +194,13 @@ namespace NOU::NOU_CORE
 	template<typename T>
 	void Error<T>::setID(sizeType id)
 	{
-		if (!empty(id))
-			m_id = id;
+		m_id = id;
 	}
 
 	template<typename T>
 	void Error<T>::setName(char32 name)
 	{
-		if (!empty(name))
-			m_name = name;
-	}
-
-	template<typename T>
-	boolean Error<T>::empty(char32 string)
-	{
-		return (string == ""); ///\Todo Warte auf Comparators und vielleicht eine trim func?
+		m_name = name;
 	}
 }
 #endif
