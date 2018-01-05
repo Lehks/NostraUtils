c18e3c8446bc9e0b8ccda7930d2c4e24ffbb88da
Lukas Reichmann <lukas@reichmann-info.de>
2018-01-05 15:40:28 +0100

    Started implementing the ThreadManager.

---
+++ Nostra Utils/src/header/nostrautils/core/Thread.hpp
@@ -0,0 +1,19 @@
+#ifndef NOU_CORE_THREAD_HPP
+#define	NOU_CORE_THREAD_HPP
+
+#include "nostrautils\core\StdIncludes.hpp"
+
+namespace NOU::NOU_CORE
+{
+	class Thread
+	{
+
+	};
+
+	class ThreadManager
+	{
+
+	};
+}
+
+#endif
