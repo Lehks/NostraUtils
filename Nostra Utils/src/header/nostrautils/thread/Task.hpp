ce4eb5ba24952599457949562f87ac951c7f4afc
Lukas Reichmann <lukas@reichmann-info.de>
2018-01-09 18:30:55 +0100

    Continued to implement the Task variations.
    Removed Thread.hpp/.cpp in the wrong folders.

--- Nostra Utils/src/header/nostrautils/thread/Task.hpp
+++ Nostra Utils/src/header/nostrautils/thread/Task.hpp
@@ -3,6 +3,9 @@
 
 #include "nostrautils\core\StdIncludes.hpp"
 #include "nostrautils\core\Meta.hpp"
+#include "nostrautils\dat_alg\Uninitialized.hpp"
+
+#include <tuple>
 
 namespace NOU::NOU_THREAD
 {
@@ -15,30 +18,73 @@ namespace NOU::NOU_THREAD
 	};
 
 	template<typename T, typename... ARGS>
-	class InvocableTask final
+	class InvocableTask final : public AbstractTask
 	{
 		static_assert(NOU_CORE::IsInvocable<T, ARGS...>::value);
 
 	public:
+		using InvocableType = T*;
 		using ReturnType = NOU_CORE::InvokeResult_t<T, ARGS...>;
 
-		T m_invocable;
-		ReturnType m_result;
+	private:
+		std::tuple<ARGS...> m_args;
+		InvocableType m_invocable;
+		NOU_DAT_ALG::Uninitialized<ReturnType> m_result;
+
+	public:
+		InvocableTask(const InvocableType &invocable, ARGS&&...args);
+		InvocableTask(InvocableType &&invocable, ARGS&&...args);
+
+		virtual void execute() override;
+
+		virtual void* getResult() override;
+	};
+
+	template<typename T, typename F, typename... ARGS>
+	class MemberFunctionTask final : public AbstractTask
+	{
+	public:
+		using StoredType = T;
+		using InvocableType = F*;
+		using ReturnType = NOU_CORE::InvokeResult_t<T, ARGS...>;
 
 	private:
-		InvocableTask(const T &invocable, ARGS&&...args);
-		InvocableTask(T &&invocable, ARGS&&...args);
+		std::tuple<ARGS...> m_args;
+		StoredType m_stored;
+		InvocableType m_invocable;
+		NOU_DAT_ALG::Uninitialized<ReturnType> m_result;
 
-		virtual void execute();
+	public:
+		MemberFunctionTask(const StoredType &stored, InvocableType invocable, ARGS&&...args);
+		MemberFunctionTask(StoredType &&stored, InvocableType invocable, ARGS&&...args);
 
-		virtual void* getResult();
+		virtual void execute() override;
+
+		virtual void* getResult() override;
 	};
 
 	template<typename T, typename... ARGS>
-	InvocableTask<T, ARGS...>::InvocableTask(const T &invocable, ARGS&&...args) :
-		m_invocable(invocable)
-	{
+	InvocableTask<T, ARGS...>::InvocableTask(const InvocableType &invocable, ARGS&&...args) :
+		m_invocable(invocable),
+		m_args(args...)
+	{}
 
+	template<typename T, typename... ARGS>
+	InvocableTask<T, ARGS...>::InvocableTask(InvocableType &&invocable, ARGS&&...args) :
+		m_invocable(NOU_CORE::move(invocable)),
+		m_args(args)
+	{}
+
+	template<typename T, typename... ARGS>
+	void InvocableTask<T, ARGS...>::execute()
+	{
+		m_result = std::apply(m_invocable, m_args);
+	}
+
+	template<typename T, typename... ARGS>
+	void* InvocableTask<T, ARGS...>::getResult()
+	{
+		return &(*m_result);
 	}
 }
 
