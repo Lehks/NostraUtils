845de4864722eb04c8e8654de250bb9c9e316929
Lukas Reichmann <lukas@reichmann-info.de>
2018-01-10 22:25:28 +0100

    Some experiments with the Task implementation.

--- Nostra Utils/src/header/nostrautils/thread/Task.hpp
+++ Nostra Utils/src/header/nostrautils/thread/Task.hpp
@@ -4,6 +4,7 @@
 #include "nostrautils\core\StdIncludes.hpp"
 #include "nostrautils\core\Meta.hpp"
 #include "nostrautils\dat_alg\Uninitialized.hpp"
+#include "nostrautils\mem_mngt\Pointer.hpp"
 
 #include <tuple>
 
@@ -46,11 +47,10 @@ namespace NOU::NOU_THREAD
 	public:
 		using StoredType = T;
 		using InvocableType = F;
-		using ReturnType = NOU_CORE::InvokeResult_t<T, ARGS...>;
+		using ReturnType = NOU_CORE::InvokeResult_t<F, T, ARGS...>;
 
 	private:
-		std::tuple<ARGS...> m_args;
-		StoredType m_stored;
+		std::tuple<T, ARGS...> m_args;
 		InvocableType m_invocable;
 		NOU_DAT_ALG::Uninitialized<ReturnType> m_result;
 
@@ -63,16 +63,65 @@ namespace NOU::NOU_THREAD
 		virtual void* getResult() override;
 	};
 
+	template<typename T, typename F, typename... ARGS>
+	class MemberFunPtrWrapper
+	{
+		static_assert(std::is_pointer_v<T>);
+
+	private:
+		T m_ptr;
+		F m_invocable;
+
+	public:
+		MemberFunPtrWrapper(T ptr, F invocable);
+		auto operator () (ARGS&&... args);
+	};
+
+	template<typename T, typename F, typename... ARGS>
+	class MemberFunctionTask<T*, F, ARGS...> final : public AbstractTask
+	{
+	public:
+		using StoredType = T*;
+		using InvocableType = F;
+		using ReturnType = NOU_CORE::InvokeResult_t<F, NOU_CORE::removeConst_t<T>, ARGS...>;
+
+	private:
+		std::tuple<StoredType, ARGS...> m_args;
+		InvocableType m_invocable;
+		NOU_DAT_ALG::Uninitialized<ReturnType> m_result;
+
+	public:
+		MemberFunctionTask(StoredType stored, InvocableType invocable, ARGS&&...args);
+
+		virtual void execute() override;
+
+		virtual void* getResult() override;
+	};
+
+	using Task = NOU_MEM_MNGT::UniquePtr<AbstractTask>;
+
+	template<typename I, typename... ARGS>
+	NOU_FUNC Task makeTaskFromInvocable(const I &invocable, ARGS&&... args);
+
+	template<typename I, typename... ARGS>
+	NOU_FUNC Task makeTaskFromInvocable(I &&invocable, ARGS&&... args);
+
+	template<typename T, typename I, typename... ARGS>
+	NOU_FUNC Task makeTaskFromMemberFunction(const T &t, I invocable, ARGS&&... args);
+
+	template<typename T, typename I, typename... ARGS>
+	NOU_FUNC Task makeTaskFromMemberFunction(T &&t, I invocable, ARGS&&... args);
+
 	template<typename T, typename... ARGS>
-	InvocableTask<T, ARGS...>::InvocableTask(const InvocableType &invocable, ARGS&&...args) :
+	InvocableTask<T, ARGS...>::InvocableTask(const InvocableType &invocable, ARGS&&... args) :
 		m_invocable(invocable),
-		m_args(args...)
+		m_args(NOU_CORE::forward<ARGS>(args)...)
 	{}
 
 	template<typename T, typename... ARGS>
 	InvocableTask<T, ARGS...>::InvocableTask(InvocableType &&invocable, ARGS&&...args) :
-		m_invocable(NOU_CORE::forward<T>(invocable)),
-		m_args(args)
+		m_invocable(invocable),
+		m_args(NOU_CORE::forward<ARGS>(args)...)
 	{}
 
 	template<typename T, typename... ARGS>
@@ -90,29 +139,99 @@ namespace NOU::NOU_THREAD
 	template<typename T, typename F, typename... ARGS>
 	MemberFunctionTask<T, F, ARGS...>::MemberFunctionTask(const StoredType &stored, InvocableType invocable, 
 		ARGS&&...args) :
-		m_stored(stored),
 		m_invocable(invocable),
-		m_args(args...)
+		m_args(std::forward_as_tuple(stored, NOU_CORE::forward<ARGS>(args)...))
 	{}
 
 	template<typename T, typename F, typename... ARGS>
 	MemberFunctionTask<T, F, ARGS...>::MemberFunctionTask(StoredType &&stored, InvocableType invocable, 
 		ARGS&&...args) :
-		m_stored(std::forward<T>(stored)),
 		m_invocable(invocable),
-		m_args(args...)
+		m_args(std::forward_as_tuple(std::forward<StoredType>(stored), NOU_CORE::forward<ARGS>(args)...))
 	{}
 
 	template<typename T, typename F, typename... ARGS>
 	void MemberFunctionTask<T, F, ARGS...>::execute()
 	{
-		m_result = std::apply(m_invocable, std::tuple_cat(std::forward_as_tuple(m_stored, m_args)));
+		//create &m_stored to avoid copy
+		m_result = std::apply(m_invocable, m_args);
 	}
 
 	template<typename T, typename F, typename... ARGS>
 	void* MemberFunctionTask<T, F, ARGS...>::getResult()
 	{
 		return &(*m_result);
+	}
+
+
+
+	template<typename T, typename F, typename... ARGS>
+	MemberFunPtrWrapper<T, F, ARGS...>::MemberFunPtrWrapper(T ptr, F invocable) :
+		m_ptr(ptr),
+		m_invocable(invocable)
+	{}
+
+	template<typename T, typename F, typename... ARGS>
+	auto MemberFunPtrWrapper<T, F, ARGS...>::operator () (ARGS&&... args)
+	{
+		return m_ptr->*m_invocable(NOU_CORE::forward<ARGS>(args)...);
+	}
+
+
+
+	template<typename T, typename F, typename... ARGS>
+	MemberFunctionTask<T*, F, ARGS...>::MemberFunctionTask(StoredType stored, InvocableType invocable,
+		ARGS&&...args) :
+		m_invocable(invocable),
+		m_args(std::forward_as_tuple(stored, NOU_CORE::forward<ARGS>(args)...))
+	{}
+
+	template<typename T, typename F, typename... ARGS>
+	void MemberFunctionTask<T*, F, ARGS...>::execute()
+	{
+		//create &m_stored to avoid copy
+		m_result = std::apply(m_invocable, m_args);
+	}
+
+	template<typename T, typename F, typename... ARGS>
+	void* MemberFunctionTask<T*, F, ARGS...>::getResult()
+	{
+		return &(*m_result);
+	}
+
+
+
+
+	template<typename I, typename... ARGS>
+	NOU_FUNC Task makeTaskFromInvocable(const I &invocable, ARGS&&... args)
+	{
+		///\todo Replace new with appropriate allocator
+		return Task(new InvocableTask<I, ARGS...>(invocable, NOU_CORE::forward<ARGS...>(args)),
+			NOU_MEM_MNGT::defaultDeleter);
+	}
+
+	template<typename I, typename... ARGS>
+	NOU_FUNC Task makeTaskFromInvocable(I &&invocable, ARGS&&... args)
+	{
+		///\todo Replace new with appropriate allocator
+		return Task(new InvocableTask<I, ARGS...>(NOU_CORE::forward<I>(invocable), NOU_CORE::forward<ARGS...>(args)),
+			NOU_MEM_MNGT::defaultDeleter);
+	}
+
+	template<typename T, typename I, typename... ARGS>
+	NOU_FUNC Task makeTaskFromMemberFunction(const T &t, I invocable, ARGS&&... args)
+	{
+		///\todo Replace new with appropriate allocator
+		return Task(new MemberFunctionTask<T, I, ARGS...>(t, invocable,
+			NOU_CORE::forward<ARGS>(args)...), NOU_MEM_MNGT::defaultDeleter);
+	}
+
+	template<typename T, typename I, typename... ARGS>
+	NOU_FUNC Task makeTaskFromMemberFunction(T &&t, I invocable, ARGS&&... args)
+	{
+		///\todo Replace new with appropriate allocator
+		return Task(new MemberFunctionTask<T, I, ARGS...>(NOU_CORE::forward<T>(t), NOU_CORE::forward<I>(invocable),
+			NOU_CORE::forward<ARGS>(args)...), NOU_MEM_MNGT::defaultDeleter);
 	}
 }
 
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,106 @@
 #include "nostrautils\core\StdIncludes.hpp"
 #include "nostrautils\mem_mngt\AllocationCallback.hpp"
 #include "nostrautils\dat_alg\Vector.hpp"
+#include "nostrautils\dat_alg\Vector.hpp"
+#include "nostrautils\thread\Task.hpp"
+#include <string>
 #include <iostream>
 
+class B
+{
+public:
+	B()
+	{
+		std::cout << "Const" << std::endl;
+	}
+
+	B(const B&)
+	{
+		std::cout << "Copy" << std::endl;
+	}
+
+	B(B&&)
+	{
+		std::cout << "Move" << std::endl;
+	}
+
+	B& operator = (const B&)
+	{
+		std::cout << "Copy Assign" << std::endl;
+
+		return *this;
+	}
+
+	B& operator = (B&&)
+	{
+		std::cout << "Move Assign" << std::endl;
+
+		return *this;
+	}
+};
+
+class A
+{
+public:
+
+	A()
+	{
+		std::cout << "Const A" << std::endl;
+	}
+
+	A(const A&)
+	{
+		std::cout << "Copy A" << std::endl;
+	}
+
+	A(A&&)
+	{
+		std::cout << "Move A" << std::endl;
+	}
+
+	~A()
+	{
+		std::cout << "Dest A" << std::endl;
+	}
+
+	A& operator = (const A&)
+	{
+		std::cout << "Copy Assign A" << std::endl;
+
+		return *this;
+	}
+
+	A& operator = (A&&)
+	{
+		std::cout << "Move Assign A" << std::endl;
+
+		return *this;
+	}
+
+	int exec(int i, int j, const B&)
+	{
+		std::cout << ":= " << j << " " << std::endl;
+		return i + j;
+	}
+};
 
 int main()
 {
+	const A a;
+	//NOU::NOU_THREAD::Task task = NOU::NOU_THREAD::makeTaskFromMemberFunction(&a, &A::exec, 5, 6, B());
+	//std::cout << typeid(NOU::NOU_THREAD::MemberFunctionTask<const A*, decltype(&A::exec), int, int, const B&>::ReturnType).name() << std::endl;
+	
+	NOU::NOU_THREAD::MemberFunPtrWrapper<const A*, decltype(&A::exec), int, int, const B&> mfpw(&a, &A::exec);
+
+	mfpw(5, 6, B());
+
+	//task->execute();
+	
+	//std::cout << *((int*)task->getResult()) << std::endl;
+
+	//using Type = NOU::NOU_CORE::InvokeResult_t<decltype(&A::exec), A, int, int, B&>;
+
+	//std::cout << typeid(Type).name() << std::endl;
+
+	std::cin.get();
 }
