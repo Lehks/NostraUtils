8524bcadfc7cf76382c3e61b3b5680898e6a62fb
Lukas Groß <lukasgross96@web.de>
2018-01-10 18:55:13 +0100

    Fixed an wrong commit.

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,27 +1,5 @@
-#include "nostrautils\core\StdIncludes.hpp"
-#include "nostrautils\mem_mngt\AllocationCallback.hpp"
-#include "nostrautils\dat_alg\Vector.hpp"
-#include "nostrautils\core\Logging.hpp"
-#include <iostream>
 
 int main()
 {
-	NOU::NOU_CORE::Logger log;
-
-	NOU::NOU_CORE::ConsoleLogger clog;
-	NOU::NOU_CORE::EventLevelCodes evc;
-
-	std::cout << evc.getLevel(0).rawStr() << std::endl;
-	std::cout << evc.getLevel(1).rawStr() << std::endl;
-	std::cout << evc.getLevel(2).rawStr() << std::endl;
-	log.pushLogger(clog);
-
-	log.logAll("Error", "Critical error in 0x000000");
-
-	clog.writeLog("Error", "Critical error in 0x000000", clog);
-	clog.writeLog("Error", "Critical error in 0x000000", clog);
-
-
-
-	std::cin.get();
+	//Do testing here!
 }
