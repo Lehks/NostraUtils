2f8575b007f72719390b775d9e366ff1a61b401a
Mahan <mahandk128@gmail.com>
2018-01-11 11:43:23 +0100

    Added BinaryHeap.hpp

---
+++ Nostra Utils/src/header/nostrautils/dat_alg/BinaryHeap.hpp
@@ -0,0 +1,15 @@
+#ifndef NOU_DAT_ALG_BINARY_HEAP_HPP
+#define NOU_DAT_ALG_BINARY_HEAP_HPP
+
+#include "nostrautils\core\StdIncludes.hpp"
+#include "nostrautils\mem_mngt\AllocationCallback.hpp"
+
+namespace NOU::NOU_DAT_ALG
+{
+
+
+
+}
+
+#endif 
+
