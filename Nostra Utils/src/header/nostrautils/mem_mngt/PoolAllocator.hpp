c8875ce50e91386867089fa8ed38bb07628a5a6d
Lukas Groß <lukasgross96@web.de>
2018-01-15 19:56:44 +0100

    Reworked everything.

--- Nostra Utils/src/header/nostrautils/mem_mngt/PoolAllocator.hpp
+++ Nostra Utils/src/header/nostrautils/mem_mngt/PoolAllocator.hpp
@@ -4,40 +4,80 @@
 #include "nostrautils\core\StdIncludes.hpp"
 #include "nostrautils\dat_alg\Vector.hpp"
 
+#include <new>
+#include <memory>
+
 namespace NOU::NOU_MEM_MNGT
 {
-	class NOU_CLASS Memory
+	template <typename T>
+	union PoolBlock
 	{
-	private:
+		T value;
+		PoolBlock<T>* nextPoolBlock;
 
-		sizeType m_memSize;
-
-		sizeType* m_memSizePtr;
-
-	public:
-		Memory(sizeType memSize);
-
-		sizeType getMemSize();
-
-		sizeType* getMemSizePtr();
+		PoolBlock() {};
+		~PoolBlock() {};
 	};
 
+	template <typename T>
 	class NOU_CLASS PoolAllocator
 	{
 	private:
+		static const sizeType POOL_ALLOCATOR_DEFAULT_SIZE = 1024;
 
-		static NOU::NOU_MEM_MNGT::GenericAllocationCallback<const Memory*> s_allocator;
+		sizeType m_size = 0;
 
-		static NOU::NOU_DAT_ALG::Vector<const Memory*> s_poolAllocator;
-
-		static sizeType *memoryBlock;
+		PoolBlock<T>* m_data = nullptr;
+		PoolBlock<T>* m_head = nullptr;
 
 	public:
+		explicit PoolAllocator(sizeType size = POOL_ALLOCATOR_DEFAULT_SIZE) :
+			m_size(size)
+		{
+			m_data = new PoolBlock<T>[size];
+			m_head = m_data;
 
-		~PoolAllocator();
-		sizeType getPoolAllocatorSize();
+			for (sizeType i = 0; i < m_size - 1; i++)
+			{
+				m_data[i].nextPoolBlock = std::addressof(m_data[i + 1]);
+			}
+			m_data[m_size - 1].nextPoolBlock = nullptr;
+		}
 
+		PoolAllocator(const PoolAllocator& other)				= delete; //Copy Constructor
+		PoolAllocator(const PoolAllocator&& other)				= delete; //Move Constructor
+		PoolAllocator& operator=(const PoolAllocator& other)	= delete; //Copy Assignment
+		PoolAllocator& operator=(const PoolAllocator&& other)	= delete; //Move Assignment
 
+		~PoolAllocator()
+		{
+			delete[] m_data;
+			m_data = nullptr;
+			m_head = nullptr;
+		}
+
+		template <typename... arguments>
+		T* allocate(arguments&&... args)
+		{
+			if (m_head == nullptr)
+			{
+				return nullptr;
+			}
+
+			PoolBlock<T>* poolBlock = m_head;
+			m_head = m_head->nextPoolBlock;
+			T* retVal = new (std::addressof(poolBlock->value)) T(std::forward<arguments>(args)...);
+			return retVal;
+		}
+
+		void deallocate(T* data)
+		{
+			///\TODO what if data is not part of m_data?
+			data->~T();
+			PoolBlock<T>* poolBlock = reinterpret_cast<PoolBlock<T>*>(data);
+			poolBlock->nextPoolBlock = m_head;
+			m_head = poolBlock;
+		}
 	};
 }
 
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,52 @@
 #include "nostrautils\core\StdIncludes.hpp"
 #include "nostrautils\mem_mngt\AllocationCallback.hpp"
 #include "nostrautils\dat_alg\Vector.hpp"
-#include <iostream>
+#include "nostrautils\mem_mngt\PoolAllocator.hpp"
 
+#include <iostream>
+#include <string>
+
+class Person
+{
+public:
+	size_t m_age;
+	std::string m_name;
+	std::string m_haircolor;
+
+	Person(size_t age, std::string name, std::string haircolor) :
+		m_age(age),
+		m_name(name),
+		m_haircolor(haircolor)
+	{
+		std::cout << "Konstruktor" << std::endl;
+	}
+
+	~Person()
+	{
+		std::cout << "Destruktor" << std::endl;
+	}
+
+	void print()
+	{
+		std::cout << m_name << " " << m_age << " " << m_haircolor << std::endl;
+	}
+};
 
 int main()
 {
+	NOU::NOU_MEM_MNGT::PoolAllocator<Person> personenAllocator;
+	while (true)
+	{
+		Person* p1 = personenAllocator.allocate(18, "Peter", "Braun");
+		Person* p2 = personenAllocator.allocate(20, "Karl", "Blond");
+
+		p1->print();
+		p2->print();
+
+		personenAllocator.deallocate(p1);
+		personenAllocator.deallocate(p2);
+	}
+	
+
+	std::cin.get();
 }
