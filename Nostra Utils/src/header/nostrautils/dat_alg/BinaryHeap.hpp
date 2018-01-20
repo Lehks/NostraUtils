4f4c1bb4b6f24bd0e5e9de331c55dc2138be3cf1
Dennis Franz <DennisFranz96@gmx.net>
2018-01-20 23:59:22 +0100

    added decreaseKey function aswell as bit magic

--- Nostra Utils/src/header/nostrautils/dat_alg/BinaryHeap.hpp
+++ Nostra Utils/src/header/nostrautils/dat_alg/BinaryHeap.hpp
@@ -12,84 +12,199 @@ namespace NOU::NOU_DAT_ALG
 	template<typename T>
 	class NOU_CLASS BinaryHeap
 	{
-	private:
-		NOU::NOU_MEM_MNGT::AllocationCallback<T>	&m_allocator;
-		NOU::boolean								isMinHeap;
-		Vector<Pair>								m_data;
 	public:
-		BinaryHeap<T>(boolean isMinHeap = true, NOU::NOU_MEM_MNGT::AllocationCallback<T> &allocator = NOU_MEM_MNGT::GenericAllocationCallback<T>::getInstance());
+		using PriorityTypePart = NOU::uint32;
+		using PriorityType = NOU::uint64;
 
-		template<typename O>
-		void enqueue(const T& data, const O& priority);
+	private:
+		boolean										isMinHeap;
 
-		template<typename O>
+		Vector<NOU::NOU_DAT_ALG::Pair<T, PriorityType>>			m_data;
+		
+		PriorityTypePart m_nextPriorityCounter;
+
+		PriorityTypePart getNextPriorityCounter();
+		PriorityType makePriority(PriorityTypePart rawPriority);
+		PriorityTypePart getPriority(PriorityType priority) const;
+
+		void checkForLaw(sizeType i = m_data.size());
+
+	public:
+		BinaryHeap(boolean isMinHeap = true, NOU::NOU_MEM_MNGT::AllocationCallback<T> &allocator = NOU_MEM_MNGT::GenericAllocationCallback<T>::getInstance());
+		BinaryHeap(const BinaryHeap<T> &other);
+		BinaryHeap(BinaryHeap<T> && otehr);
+
+		void enqueue(const T& data, PriorityTypePart priority);
+
 		void dequeue();
+
+		void decreaseKey(PriorityType priority, PriorityType newpriority);
 
 		sizeType size() const;
 	};
 
 	template<typename T>
-	BinaryHeap<T>::BinaryHeap(boolean isMinHeap = true, NOU::NOU_MEM_MNGT::AllocationCallback<T>& allocator = NOU_MEM_MNGT::GenericAllocationCallback<T>::getInstance()) :
-		isMinHeap(isMinHeap),
-		m_allocator(allocator),
-		m_data(0)
-	{}
+	typename BinaryHeap<T>::PriorityTypePart BinaryHeap<T>::getNextPriorityCounter()
+	{
+		return ++m_nextPriorityCounter;
+	}
 
 	template<typename T>
-	template<typename O>
-	void BinaryHeap<T>::enqueue(const T& data, const O& priority)
+	typename BinaryHeap<T>::PriorityType BinaryHeap<T>::makePriority(PriorityTypePart rawPriority)
 	{
-		NOU::NOU_DAT_ALG::Pair<T,O> p(data, priority);
+		PriorityType ret = rawPriority; //priority left, counter right
 
-		m_data.pushBack(p);
+		ret |= getNextPriorityCounter() << (sizeof(PriorityTypePart) * 8);
 
-		sizeType i = m_data.size() - 1;
-		while (1 > 0)
+		return ret;
+	}
+
+	template<typename T>
+	typename BinaryHeap<T>::PriorityTypePart BinaryHeap<T>::getPriority(PriorityType priority) const
+	{
+		return static_cast<PriorityTypePart>(priority & 0xFFFFFFFF); //0x FFFF FFFF : first 32 bits are 1, remaining bits are 0
+	}
+
+	template<typename T>
+	void BinaryHeap<T>::checkForLaw(sizeType i)
+	{
+		if (isMinHeap)
 		{
-			sizeType a = (i - 1) / 2;
+			while (1 > 0)
+			{
+				sizeType p = static_cast<int>(i - 1) / 2;
 
-			if (m_data[i].dataTwo < m_data[p].dataTwo)
-			{
-				swap(m_data[i], m_data[p]);
+				if (getPriority(m_data[i].dataTwo) < getPriority(m_data[p].dataTwo))
+				{
+					swap(m_data[i], m_data[p]);
+				}
+				else
+				{
+					break;
+				}
 			}
-			else
+		}
+		else
+		{
+			while (1 > 0)
 			{
-				break;
+				sizeType p = static_cast<int>(i - 1) / 2;
+
+				if (getPriority(m_data[i].dataTwo) > getPriority(m_data[p].dataTwo))
+				{
+					swap(m_data[i], m_data[p]);
+				}
+				else
+				{
+					break;
+				}
 			}
 		}
 	}
 
 	template<typename T>
-	template<typename O>
+	BinaryHeap<T>::BinaryHeap(boolean isMinHeap, NOU::NOU_MEM_MNGT::AllocationCallback<T>& allocator) :
+		isMinHeap(isMinHeap),
+		m_data(0, allocator),
+		m_nextPriorityCounter(-1) //counter still starts at 0
+	{}
+
+	template<typename T>
+	BinaryHeap<T>::BinaryHeap(const BinaryHeap<T> &other) :
+		isMinHeap(other.isMinHeap),
+		m_data(other.m_data),
+		m_nextPriorityCounter(other.m_nextPriorityCounter)
+	{}
+
+	template<typename T>
+	BinaryHeap<T>::BinaryHeap(BinaryHeap &&other) :
+		isMinHeap(other.isMinHeap),
+		m_data(other.m_data),
+		m_nextPriorityCounter(other.m_nextPriorityCounter)
+	{}
+
+	template<typename T>
+	void BinaryHeap<T>::enqueue(const T& data, PriorityTypePart priority)
+	{
+		
+		NOU::NOU_DAT_ALG::Pair<T, PriorityType> p(data, makePriority(priority));
+
+		m_data.pushBack(p);
+
+		checkForLaw();
+	}
+
+	template<typename T>
 	void BinaryHeap<T>::dequeue()
 	{
 		m_data.remove(0);
 
 		sizeType i = m_data.size() - 1;
-		while (1 < n / 2 - 2)
+
+		if (isMinHeap)
 		{
-			sizeType l = 2 * i + 1;
-			sizeType r = 2 * i + 2;
-
-			sizeType s = l;
-
-			if (m_data[r].dataTwo < m_data[l].dataTwo)
-				c = r;
-			
-			if (m_data[c].dataTwo < m_data[i].dataTwo)
+			while (1 < n / 2 - 2)
 			{
-				swap(m_data[c], m_data[i]);
-				i = c;
-			}
-			else
-			{
-				break;
-			}
+				sizeType l = 2 * i + 1;
+				sizeType r = 2 * i + 2;
 
+				sizeType s = l;
+
+				if (getPriority(m_data[r].dataTwo) < getPriority(m_data[l].dataTwo))
+					c = r;
+
+				if (getPriority(m_data[r].dataTwo) < getPriority(m_data[l].dataTwo))
+				{
+					swap(m_data[c], m_data[i]);
+					i = c;
+				}
+				else
+				{
+					break;
+				}
+
+			}
+		}
+		else
+		{
+			while (1 < n / 2 - 2)
+			{
+				sizeType l = 2 * i + 1;
+				sizeType r = 2 * i + 2;
+
+				sizeType s = l;
+
+				if (getPriority(m_data[r].dataTwo) > getPriority(m_data[l].dataTwo))
+					c = r;
+
+				if (getPriority(m_data[r].dataTwo) > getPriority(m_data[l].dataTwo))
+				{
+					swap(m_data[c], m_data[i]);
+					i = c;
+				}
+				else
+				{
+					break;
+				}
+
+			}
 		}
 
 	}
 
+	template<typename T>
+	void BinaryHeap<T>::decreaseKey(PriorityType priority , PriorityType newpriority)
+	{
+		for (int i = 0; i < m_data.size(); i++)
+		{
+			if (m_data.at(i).dataTwo == priority)
+			{
+				m_data.at(i).dataTwo = newpriority;
+				checkForLaw(i);
+				break;
+			}
+		}
+	}
 
 	template<typename T>
 	sizeType BinaryHeap<T>::size() const
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,6 +1,7 @@
 #include "nostrautils\core\StdIncludes.hpp"
 #include "nostrautils\mem_mngt\AllocationCallback.hpp"
 #include "nostrautils\dat_alg\Vector.hpp"
+#include "nostrautils\dat_alg\BinaryHeap.hpp"
 #include <iostream>
 
 
