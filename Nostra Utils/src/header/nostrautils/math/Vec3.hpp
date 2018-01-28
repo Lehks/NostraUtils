4b498c9d7219a9f4c3778f0763a6f3bddd2455b3
eaxs <tkuhn@live.com>
2018-01-28 22:18:52 +0100

    Updated Vec3 with new methods. Implementation pending

--- Nostra Utils/src/header/nostrautils/math/Vec3.hpp
+++ Nostra Utils/src/header/nostrautils/math/Vec3.hpp
@@ -11,13 +11,19 @@ namespace NOU::NOU_MATH
 			float32 m_data[3];
 
 		public:
-			Vec3(float32 x, float32 y, float32 z);
+			Vec3(const float32 x, const float32 y, const float32 z);
 			Vec3 add(const Vec3 & vec);
 			Vec3 sub(const Vec3 & vec);
-			Vec3 mult(const Vec3 & vec);
+			float32 dot(const Vec3 & vec);
+			Vec3 cross(const Vec3 & vec);
+			Vec3 mult(const float32);
 		    Vec3 div(const Vec3 & vec);
 
 			Vec3 operator + (const Vec3 & vec);
+			Vec3 operator - (const Vec3 & vec);
+			Vec3 operator * (const Vec3 & vec);
+			Vec3 operator * (const float32 num);
+			Vec3 operator / (const Vec3 & vec);
 	};
 }
 
--- Nostra Utils/src/source/nostrautils/math/Vec3.cpp
+++ Nostra Utils/src/source/nostrautils/math/Vec3.cpp
@@ -4,47 +4,53 @@
 
 namespace NOU::NOU_MATH 
 {
-	/*int a, b, c, d, e, f;
-	
-	int vec1[3] = { a,b,c };
-	int vec2[3] = { d,e,f };
-	*/
-	Vec3::Vec3(float32 x, float32 y, float32 z) :
+	Vec3::Vec3(const float32 x, const float32 y, const float32 z) :
 		m_data{x, y, z}
 	{
 		
 	}
 
 
-	Vec3 Vec3::add(const Vec3 &vec)
-	{
+	Vec3 Vec3::add(const Vec3 & vec)
+	{ 
 		return Vec3(m_data[0] + vec.m_data[0],
 			m_data[1] + vec.m_data[1], m_data[2] + vec.m_data[2]);
 	}
 
 
-	Vec3 Vec3::sub(const Vec3 &vec)
+	Vec3 Vec3::sub(const Vec3 & vec)
 	{
 		return Vec3(m_data[0] - vec.m_data[0],
 			m_data[1] - vec.m_data[1], m_data[2] - vec.m_data[2]);
 	}
 
 
-	Vec3 Vec3::mult(const Vec3 &vec)
+	float32 Vec3::dot(const Vec3 & vec)
 	{
-		return Vec3(m_data[0] * vec.m_data[0],
-			m_data[1] * vec.m_data[1], m_data[2] * vec.m_data[2]);
+		// Todo
 	}
 
-	Vec3 Vec3::div(const Vec3 &vec)
+
+	Vec3 Vec3::div(const Vec3 & vec)
 	{ 
 		return Vec3(m_data[0] / vec.m_data[0],
 			m_data[1] / vec.m_data[1], m_data[2] / vec.m_data[2]);
 	}
 
 
-	Vec3 Vec3::operator+(const Vec3 &vek)
+	Vec3 Vec3::operator + (const Vec3 & vek)
 	{
 		return add(vek);
+	}
+
+
+	Vec3 Vec3::operator - (const Vec3 & vek)
+	{
+		return sub(vek);
+	}
+
+	Vec3 Vec3::operator / (const Vec3 & vek)
+	{
+		return div(vek);
 	}
 }
