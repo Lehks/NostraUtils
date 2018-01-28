2e8eaeeb4af0830616bcc0a1d97325d6448df9ed
gaetanredington <gaetanredington@gmail.com>
2018-01-28 22:02:00 +0100

    Added Vec3 and Matrix3 classes

---
+++ Nostra Utils/src/header/nostrautils/math/Matrix3.hpp
@@ -0,0 +1,28 @@
+#ifndef NOU_MATH_MATRIX3_HPP
+#define NOU_MATH_MATRIX3_HPP
+
+#include "nostrautils\core\StdIncludes.hpp"
+#include "nostrautils\math\Vec3.hpp"
+
+namespace NOU::NOU_MATH
+{
+	class NOU_CLASS Matrix3
+	{
+		public:
+			int32 m_cols;
+			Vec3 m_data[];
+
+		public:
+			Matrix3(int32 cols);
+			Matrix3 add(const Matrix3 & matrix);
+			Matrix3 sub(const Matrix3 & matrix);
+			Matrix3 mult(const Matrix3 & matrix);
+			Matrix3 div(const Matrix3 & matrix);
+			int32 size();
+
+			Matrix3 operator + (const Matrix3 & matrix);
+	};
+}
+
+#endif
+
---
+++ Nostra Utils/src/source/nostrautils/math/Matrix3.cpp
@@ -0,0 +1,13 @@
+#include "nostrautils\math\Vec3.hpp"
+#include "nostrautils\math\Matrix3.hpp"
+#include "nostrautils\core\StdIncludes.hpp"
+
+
+namespace NOU::NOU_MATH
+{
+	Matrix3::Matrix3(int32 cols)
+	{
+		this->m_cols = cols;
+		this->m_data[cols] = Vec3(0.0, 0.0, 0.0);
+	}
+}
