5cb55bf8d3df9935fd8cb960b084e2a6b3228832
Lukas Groß <lukasgross96@web.de>
2018-02-02 15:29:10 +0100

    Started implementing the GeneralPurposeAllocator.

---
+++ Nostra Utils/src/header/nostrautils/mem_mngt/GeneralPurposeAllocator.hpp
@@ -0,0 +1,107 @@
+#ifndef NOU_MEM_MNGT_GENERAL_PURPOSE_ALLOCATOR_HPP
+#define NOU_MEM_MNGT_GENERAL_PURPOSE_ALLOCATOR_HPP
+
+#include "nostrautils\core\StdIncludes.hpp"
+#include "nostrautils\core\Utils.hpp"
+#include "nostrautils\mem_mngt\Utils.hpp"
+
+namespace NOU::NOU_MEM_MNGT
+{
+	namespace NOU::NOU_MEM_MNGT::INTERNAL
+	{
+		template <typename T>
+		T nextMultiple(T multipleOf, T value)
+		{
+			T multiple = value + multipleOf - 1;
+			multiple -= (multiple % multipleOf);
+			return multiple;
+		}
+
+		class GeneralPurposeAllocatoFreeChunk
+		{
+		public:
+			byte* m_addr;
+			sizeType m_size;
+
+			GeneralPurposeAllocatoFreeChunk(byte* addr, sizeType size) :
+				m_addr(addr),
+				m_size(size)
+			{
+				//do nothing
+			}
+
+			boolean touches(const GeneralPurposeAllocatoFreeChunk& other) const
+			{
+				if (m_addr + m_size == other.m_addr)
+				{
+					return true;
+				}
+
+				if (other.m_addr + other.m_size == m_addr)
+				{
+					return true;
+				}
+
+				return false;
+			}
+
+			boolean operator>(const GeneralPurposeAllocatoFreeChunk& other) const
+			{
+				return m_addr > other.m_addr;
+			}
+
+			boolean operator<(const GeneralPurposeAllocatoFreeChunk& other) const
+			{
+				return m_addr < other.m_addr;
+			}
+
+			boolean operator>=(const GeneralPurposeAllocatoFreeChunk& other) const
+			{
+				return m_addr >= other.m_addr;
+			}
+
+			boolean operator<=(const GeneralPurposeAllocatoFreeChunk& other) const
+			{
+				return m_addr <= other.m_addr;
+			}
+
+			boolean operator==(const GeneralPurposeAllocatoFreeChunk& other) const
+			{
+				return m_addr == other.m_addr;
+			}
+
+			template <typename T, typename... arguments>
+			T* allocateObject(sizeType amountofObjects = 1, arguments&&... args)
+			{
+				static_assert(alignof(T) <= 128, "Max alignment of 128 was exceeded!");
+				byte* allocationLocation = (byte*)nextMultiple(alignof(T), ((sizeType)m_addr) + 1);
+				sizeType amountOfBytes = amountofObjects * sizeof(T);
+				byte* newAddr = allocationLocation + amountOfBytes;
+
+				if (newAddr <= m_addr + m_size)
+				{
+					byte offset = (byte)(allocationLocation - m_addr);
+					allocationLocation[-1] = offset;
+
+					T* returnPointer = reinterpret_cast<T*>(allocationLocation);
+					m_size -= newAddr - m_addr;
+					m_addr = newAddr;
+
+					for (sizeType i = 0; i < amountofObjects; i++)
+					{
+						T* object = NOU_MEM_MNGT::addressof(returnPointer[i]);
+						new(object) T(NOU_CORE::forward<arguments>(args)...);
+					}
+
+					return returnPointer;
+				}
+				else
+				{
+					return nullptr;
+				}
+			}
+		};
+	}
+}
+
+#endif
