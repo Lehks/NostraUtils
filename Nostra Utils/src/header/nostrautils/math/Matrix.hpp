2a368ab91cd001024812581f7d69c343891b47af
eaxs <tkuhn@live.com>
2018-03-18 21:38:35 +0100

    Added matrix default constructor

--- Nostra Utils/src/header/nostrautils/math/Matrix.hpp
+++ Nostra Utils/src/header/nostrautils/math/Matrix.hpp
@@ -6,25 +6,27 @@
 
 namespace NOU::NOU_MATH
 {
-	template<uint32 N, uint32 M, typename T = float32>
+	template<uint32 R, uint32 C, typename T = float32>
 	class NOU_CLASS Matrix
 	{
 		public:
 			using Type = T;
-			using Rows = N;
-			using Columns = M;
 
 		private:
-			Vec<Rows, Type> m_data[Columns];
+			Vec<R, Type> m_data[C];
 
 		public:
 			Matrix();
 	};
 
-	template<uint32 N, uint32 M, typename T>
-	Matrix<N, M, T>::Matrix()
+	template<uint32 R, uint32 C, typename T>
+	Matrix<R, C, T>::Matrix()
 	{
-		
+		// Fill Matrix with vectors of value 0.
+		for (i = 0; i < C; i++)
+		{
+			m_data[i] = Vec<R, T>();
+		}
 	}
 }
 
