c9deb96fe4b36248af3e66e9dc28f5d19bb3dcf3
Dennis Franz <dennisfranz96@gmx.net>
2018-03-22 12:31:40 +0100

    Created FileManager

---
+++ Nostra Utils/src/header/nostrautils/file_mngt/FileManager.hpp
@@ -0,0 +1,19 @@
+#ifndef NOU_FILE_MNGT_FILEMANAGER_HPP
+#define NOU_FILE_MNGT_FILEMANAGER_HPP
+
+#include "nostrautils\core\StdIncludes.hpp"
+
+namespace NOU::NOU_FILE_MNGT
+{
+
+	class NOU_CLASS FileManager
+	{
+	private	:
+
+	public:
+
+	};
+
+}
+
+#endif
