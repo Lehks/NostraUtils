c393556363ed7ae38bb3ab3464fe42c991fecf4c
Dennis Franz <dennisfranz96@gmx.net>
2018-03-30 18:04:25 +0200

    FIxed Include

--- UnitTests/unittests.cpp
+++ UnitTests/unittests.cpp
@@ -1618,8 +1618,6 @@ namespace UnitTests
 			NOU::NOU_FILE_MNGT::Path p13 = "D:\\Users\\";
 			NOU::NOU_FILE_MNGT::Path p14 = "D:\\Users\\SomeOtherDir";
 
-			NOU::NOU_FILE_MNGT::Path cwd = NOU::NOU_FILE_MNGT::Path::currentWorkingDirectory();
-
 			Assert::IsTrue(p.getName() == "testfile");
 			Assert::IsTrue(p1.getName() == "testfile");
 			Assert::IsTrue(p2.getName() == "testfile");
@@ -1648,14 +1646,7 @@ namespace UnitTests
 			Assert::IsTrue(p11.getParentPath() == "C:\\Users\\Dennis\\TestDir\\MyFolder");
 			Assert::IsTrue(p12.getParentPath() == "C:\\Users\\Dennis");
 			Assert::IsTrue(p13.getParentPath() == "D:");
-			Assert::IsTrue(p14.getParentPath() == "D:\\Users"); 
-
-			Assert::IsTrue(p10.getRelativePath() == "C:\\Users\\Dennis\\TestDir");
-			Assert::IsTrue(p11.getRelativePath() == "C:\\Users\\Dennis\\TestDir\\MyFolder");
-			Assert::IsTrue(p12.getRelativePath() == "C:\\Users\\Dennis"); 
-			Assert::IsTrue(p13.getRelativePath() == "..\\..\\Users");
-			Assert::IsTrue(p14.getRelativePath() == "..\\..\\Users\\SomeOtherDir"); 
-			Assert::IsTrue(cwd.getRelativePath() == ".");
+			Assert::IsTrue(p14.getParentPath() == "D:\\Users");  
 		}
 	};	
 }
