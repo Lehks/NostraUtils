053b64d432e706191aaaba007f9618be5e961677
Lukas Reichmann <lukas@reichmann-info.de>
2018-04-05 19:21:12 +0200

    Added functions to query current RAM usage.

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,42 +1,23 @@
 #include "nostrautils\core\StdIncludes.hpp"
 #include "nostrautils\mem_mngt\AllocationCallback.hpp"
 #include "nostrautils\dat_alg\ObjectPool.hpp"
+#include "nostrautils\dat_alg\Vector.hpp"
 #include "nostrautils\file_mngt\File.hpp"
 #include "nostrautils\file_mngt\Path.hpp"
+#include "nostrautils/core/SystemInformation.hpp"
 #include <iostream>
 
 using namespace std;
 using namespace NOU;
+using namespace NOU_CORE;
 //using namespace NOU_CORE;
 using namespace NOU_DAT_ALG;
 using namespace NOU_FILE_MNGT;
 
 int main()
 {
-	String8 sv = "Hello World!";
-	String8 s = sv.substring(0, 5);
-
-	std::cout << s.rawStr() << std::endl;
-
-	NOU::NOU_FILE_MNGT::Path cwd = NOU::NOU_FILE_MNGT::Path::currentWorkingDirectory();
-
-	NOU::NOU_DAT_ALG::String8 str = cwd.getAbsolutePath();
-	str.append("\\Test\\Dennis\\WasGehtAb");
-
-	std::cout << str.rawStr() << std::endl;
-
-	NOU::NOU_FILE_MNGT::Path p15 = str;
-
-	std::cout << Path::currentWorkingDirectory().getAbsolutePath().rawStr() << std::endl;
-
-	std::cout << p15.getRelativePath().rawStr() << std::endl;
-
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-	
-
-	std::cout << std::endl;
+	std::cout << SystemInformation::currentlyUsedMemory() << std::endl;
+	std::cout << SystemInformation::totalPhysicalMemory() << std::endl;
 
 	std::cin.get();
 }							
