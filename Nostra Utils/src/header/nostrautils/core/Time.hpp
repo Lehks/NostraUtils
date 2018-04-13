8a86c181e30134283a13124eecf5ba3572607d93
Leslie <leslie@bmarxen.de>
2018-04-13 08:39:27 +0200

    Implemented a function to return the current time in ms

--- Nostra Utils/src/header/nostrautils/core/Time.hpp
+++ Nostra Utils/src/header/nostrautils/core/Time.hpp
@@ -1,8 +1,21 @@
 #ifndef NOU_CORE_TIME_HPP
 #define NOU_CORE_TIME_HPP
+#include "nostrautils/core/StdIncludes.hpp"
+
+/** \file Vector.hpp
+\author  Leslie Marxen
+\brief   This file provides some functionallity to work with time values and constants.
+*/
 
 
 
+namespace NOU::NOU_CORE {
 
+	/**
+	\brief returns the UNIX epoch time in ms
+	\return returns the Time since the UNIX epoch in milliseconds
+	*/
 
+	uint32 NOU_FUNC currentTimeMillis();
+}
 #endif
