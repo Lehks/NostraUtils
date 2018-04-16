dd759a3c863baa99e146578ea71181a565a221b5
Lukas Groß <lukasgross96@web.de>
2018-04-16 22:09:52 +0200

    Removed double definition in the unittests.

--- Unittests/unittests.cpp
+++ Unittests/unittests.cpp
@@ -1860,41 +1860,6 @@ TEST_METHOD(Logging)
 	log->pushLogger<TestLogger>();
 	log->write(NOU::NOU_CORE::EventLevelCodes::DEBUG, "Unittest error.");
 
-	if (testOutput.size() == writeOutput.size()) //For better error message
-	{
-		for (int i = 0; i < testOutput.size(); i++)
-		{
-			IsTrue(testOutput.at(i) == writeOutput.at(i));
-		}
-	}
-	else
-	{
-		IsTrue(false);
-	}
-
-	NOU_CHECK_ERROR_HANDLER;
-}
-
-TEST_METHOD(Logging)
-{
-	static NOU::NOU_CORE::Event testEvent(NOU::NOU_CORE::EventLevelCodes::DEBUG, "Unittest error.");
-
-	static NOU::NOU_DAT_ALG::String8 testOutput = NOU::NOU_CORE::Logger::print(testEvent);
-	static NOU::NOU_DAT_ALG::String8 writeOutput;
-
-	NOU::NOU_CORE::Logger* log = NOU::NOU_CORE::Logger::instance();
-
-	class TestLogger : public NOU::NOU_CORE::ILogger
-	{
-		void write(const NOU::NOU_CORE::Event& event, StringType filename)
-		{
-			writeOutput = NOU::NOU_CORE::Logger::print(event);
-		}
-	};
-
-	log->pushLogger<TestLogger>();
-	log->write(NOU::NOU_CORE::EventLevelCodes::DEBUG, "Unittest error.");
-
 	using namespace std::chrono_literals;
 	std::this_thread::sleep_for(1s); //Thread is performing operation. Because of that it is necessary at 
 									 //this point to make a short sleep to let the thread perform the write
