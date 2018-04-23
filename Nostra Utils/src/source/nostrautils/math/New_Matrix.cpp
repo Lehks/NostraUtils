dff48a897b314eb2d16214f2f9b1295e7c75944c
gaetanredington <gaetanredington@gmail.com>
2018-04-23 08:10:10 +0200

    new matrix with array

--- Nostra Utils/src/source/nostrautils/math/New_Matrix.cpp
+++ Nostra Utils/src/source/nostrautils/math/New_Matrix.cpp
@@ -1,17 +1,20 @@
-//#include "stdafx.h"
+
 #include <iostream>
 
 
-int ** generateMatrix(int rows, int cols) {
-	int **temp = new int*[rows];
+int **generateMatrix(int rows, int cols) {
+
+	int** temp = new int*[rows];
 
 	for (int i = 0; i < rows; ++i) {
 		temp[i] = new int[cols];
 	}
+
 	return temp;
 }
 
 void printMatrix(int** matrix, int rows, int cols) {
+
 	for (int i = 0; i < rows; ++i) {
 		for (int j = 0; j < cols; ++j) {
 			std::cout << matrix[i][j] << " ";
@@ -20,10 +23,10 @@ void printMatrix(int** matrix, int rows, int cols) {
 	}
 }
 
-void populateMatrix(int** matrix, int rows, int cols, int *src, int src_size) {
+void populateMatrix(int** matrix, int rows, int cols, int * src, int src_size) {
 
 	if (rows * cols != src_size) {
-		std::cout << "Size of the matrix is not equal to the size of source Array" << std::endl;
+		std::cout << "Size of matrix is not equal to the size of source array" << std::endl;
 		exit(-1);
 	}
 
@@ -35,7 +38,7 @@ void populateMatrix(int** matrix, int rows, int cols, int *src, int src_size) {
 	}
 }
 
-int ** addMatrix(int** matA, int** matB, int rows, int cols) {
+int** addMatrix(int** matA, int** matB, int rows, int cols) {
 	int** resultMat = generateMatrix(rows, cols);
 
 	for (int i = 0; i < rows; ++i) {
@@ -47,30 +50,75 @@ int ** addMatrix(int** matA, int** matB, int rows, int cols) {
 	return resultMat;
 }
 
+int** subMatrix(int** matA, int** matB, int rows, int cols) {
+	int** resultMat = generateMatrix(rows, cols);
 
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			resultMat[i][j] = matA[i][j] - matB[i][j];
+		}
+	}
+
+	return resultMat;
+}
 /*
+int** mulMatrix(int** matA, int** matB, int rows, int cols) {
+if(cols != )
+
+int** resultMat = generateMatrix(rows, cols);
+
+for (int i = 0; i < rows; ++i) {
+for (int j = 0; j < cols; ++j) {
+resultMat[i][j] = matA[i][j] - matB[i][j];
+}
+}
+
+return resultMat;
+}
+*/
+void transpose(int** matTrans, int rows, int cols) {
+	int **temp = generateMatrix(cols, rows);
+
+	for (int i = 0; i < cols; ++i) {
+		for (int j = 0; j < rows; ++j) {
+			temp[i][j] = matTrans[i][j];
+		}
+	}
+
+	for (int i = 0; i < rows; ++i) {
+		delete[] matTrans[i];
+	}
+	delete[] matTrans;
+
+	matTrans = temp;
+	int tmp = cols;
+	rows = cols;
+	cols = tmp;
+}
+
+
+
 int main() {
 
-	//first matrix
-	int m1_src[] = { 0, 0, 1, 0, 1, 0, 1, 0, 0 };
+	int m1_src[] = { 1,1,1,2,2,2,3,3,3 };
 	int m1_rows = 3, m1_cols = 3;
 	int** m1 = generateMatrix(m1_rows, m1_cols);
 	populateMatrix(m1, m1_rows, m1_cols, m1_src, 9);
+	printMatrix(m1, m1_rows, m1_cols);
+	std::cout << std::endl;
 
-	//second mmatrix
-	int m2_src[] = { 1, 1, 1, 2, 2, 2, 3, 3, 3 };
+	int m2_src[] = { 1,1,1,2,2,2,3,3,3 };
 	int m2_rows = 3, m2_cols = 3;
 	int** m2 = generateMatrix(m2_rows, m2_cols);
 	populateMatrix(m2, m2_rows, m2_cols, m2_src, 9);
-
-	//printMatrix(m1, m1_rows, m1_cols);
+	printMatrix(m2, m2_rows, m2_cols);
 	std::cout << std::endl;
-	//printMatrix(m2, m2_rows, m2_cols);
 
-	//result matrix
+	// result
 	printMatrix(addMatrix(m1, m2, m1_rows, m1_cols), m1_rows, m1_cols);
+
+	printMatrix(subMatrix(m1, m2, m1_rows, m1_cols), m1_rows, m1_cols);
 
 	system("pause");
 	return 0;
 }
-*/
