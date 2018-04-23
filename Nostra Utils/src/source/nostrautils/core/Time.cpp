a1a2eb1140d0920c4004b7834696a12df1ac573d
Leslie <leslie@bmarxen.de>
2018-04-23 13:52:03 +0200

    implemented currentTimeNanos

--- Nostra Utils/src/source/nostrautils/core/Time.cpp
+++ Nostra Utils/src/source/nostrautils/core/Time.cpp
@@ -1,8 +1,15 @@
 #include "nostrautils/core/Time.hpp"
 #include <chrono>
 
-namespace NOU::NOU_CORE {
-	uint32 currentTimeMillis() {
+namespace NOU::NOU_CORE
+{
+	uint32 currentTimeMillis()
+	{
 		return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+	}
+
+	uint64 currentTimeNanos()
+	{
+		return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock.now().time_since_epoch()).count();
 	}
 }
