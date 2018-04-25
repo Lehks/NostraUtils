048457ea8462165428f1582a4c0091e1f8048f43
Leslie <leslie@bmarxen.de>
2018-04-25 11:15:33 +0200

    added errorhandling

--- Nostra Utils/src/source/nostrautils/file_mngt/File.cpp
+++ Nostra Utils/src/source/nostrautils/file_mngt/File.cpp
@@ -196,6 +196,13 @@ namespace NOU::NOU_FILE_MNGT
 	boolean File::deleteFile()
 	{
 		int err = remove(m_path.getAbsolutePath().rawStr());
-		return err == 0;
+		if(err == 0)
+		{
+			return true;
+		}else
+		{
+			NOU_PUSH_ERROR(NOU_CORE::getErrorHandler(), NOU_CORE::ErrorCodes::INVALID_OBJECT, "Could not delete File");
+			return false;
+		}
 	}
 }
