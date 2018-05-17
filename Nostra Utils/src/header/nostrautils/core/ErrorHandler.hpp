05a929f70ed8eb35dea45ba850b14a4e1278d541
Lukas Groß <lukasgross96@web.de>
2018-05-17 23:48:59 +0200

    Added check for the existence of a file and added additional ErrorCode.

--- Nostra Utils/src/header/nostrautils/core/ErrorHandler.hpp
+++ Nostra Utils/src/header/nostrautils/core/ErrorHandler.hpp
@@ -485,6 +485,11 @@ namespace NOU::NOU_CORE
 			INVALID_OBJECT,
 
 			/**
+			\brief Cannot open the specified file.
+			*/
+			CANNOT_OPEN_FILE,
+
+			/**
 			\brief	Not an actual error, but always the last element in the enum. The error codes 0 - 
 					LAST_ELEMENT are always reserved for the default error pool.
 			*/
