258901736a73a330b5a4cc42dbfb86483ce48ca4
Leslie <leslie@bmarxen.de>
2018-05-17 22:58:01 +0200

    Added definition for move constructor#

--- Nostra Utils/src/header/nostrautils/dat_alg/HashMapTest.hpp
+++ Nostra Utils/src/header/nostrautils/dat_alg/HashMapTest.hpp
@@ -193,13 +193,14 @@ namespace NOU::NOU_DAT_ALG
 	template<typename K, typename V>
 	HashMapT<K, V>::HashMapT(const HashMapT &other)
 	{
-		
+		this-> m_size = other.size();
+		this-> m_data = other.m_data;
 	}
 
 	template <typename K, typename V>
 	HashMapT<K, V>::HashMapT(HashMapT &&other)
 	{
-
+		
 	}
 
 	template <typename K, typename V>
