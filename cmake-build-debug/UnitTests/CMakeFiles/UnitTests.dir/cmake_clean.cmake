cd0201eb6731737b2da67a84cf4f210a62f99323
Dennis Franz <dennisfranz96@gmx.net>
2018-05-18 13:57:22 +0200

    Fixed Error in Path.cpp

---
+++ cmake-build-debug/UnitTests/CMakeFiles/UnitTests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnitTests.dir/unittests.cpp.obj"
+  "CMakeFiles/UnitTests.dir/DebugClass.cpp.obj"
+  "UnitTests.pdb"
+  "UnitTests.exe"
+  "UnitTests.exe.manifest"
+  "UnitTests.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnitTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
