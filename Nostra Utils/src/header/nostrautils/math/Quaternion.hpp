5cae7fc9ed7db1853ac896034df7d7778393a212
gaetanredington <gaetanredington@gmail.com>
2018-06-03 11:42:41 +0200

    Quaternions Update

--- Nostra Utils/src/header/nostrautils/math/Quaternion.hpp
+++ Nostra Utils/src/header/nostrautils/math/Quaternion.hpp
@@ -1,4 +1,4 @@
-#ifndef NOU_MATH_QUATERNION
+  #ifndef NOU_MATH_QUATERNION
 #define NOU_MATH_QUATERNION
 
 #include "nostrautils/core/StdIncludes.hpp"
@@ -22,7 +22,7 @@ namespace NOU::NOU_MATH
 
 
 	template<typename T, sizeType N, sizeType R, sizeType C>
-	class Quaternion {
+	class Quaternion : public Vector<T, N> {
 	private:
 		T m_x;
 		T m_y;
@@ -30,18 +30,18 @@ namespace NOU::NOU_MATH
 		T m_w;
 
 	public:
-		Quaternion(T x, T y, T z, T w);
-		Quaternion(Vector<T, N> axis, T angle);
-		Quaternion Normalized();
-		Quaternion Conjugate();
-		Quaternion mul(T r);
-		Quaternion mul(Quaternion r);
-		Quaternion mul(Vector<T, N> r);
-		Quaternion sub(Quaternion r);
-		Quaternion add(Quaternion r);
-		Matrix<T, R, C> ToRotationMatrix();
-		T dot(Quaternion r);
-		Quaternion NLerp(Quaternion dest, T lerpFactor, T shortest);
+		Quaternion(T &x, T &y, T &z, T &w);
+		Quaternion(Vector<T, N> &axis, T &angle);
+		Quaternion normalized();
+		Quaternion conjugate();
+		Quaternion mul(const T &r);
+		Quaternion mul(const Quaternion &r);
+		Quaternion mul(const Vector<T, N> &r);
+		Quaternion sub(const Quaternion &r);
+		Quaternion add(const Quaternion &r);
+		Matrix<T, R, C> toRotationMatrix();
+		T dot(const Quaternion &r);
+		Quaternion nLerp(Quaternion dest, T lerpFactor, T shortest);
 		Quaternion(Matrix<T, R, C> rot);
 		Vector<T, N> getForward();
 		Vector<T, N> getUp();
@@ -59,7 +59,7 @@ namespace NOU::NOU_MATH
 
 
 	template<typename T>
-	Quaternion::Quaternion(T x, T y, T z, T w) {
+	Quaternion::Quaternion(T &x, T &y, T &z, T &w) {
 		m_x = x; // Initialisierung der X Axe
 		m_y = y; // Initialisierung der Y Axe
 		m_z = z; // Initialisierung der Z Axe
@@ -67,7 +67,7 @@ namespace NOU::NOU_MATH
 	}
 
 	template<typename T, sizeType N>
-	Quaternion::Quaternion(Vector<T, N> axis, T angle)
+	Quaternion::Quaternion(Vector<T, N> &axis, T &angle)
 	{
 		T sinHalfAngle = sin(angle / 2);
 		T cosHalfAngle = cos(angle / 2);
@@ -85,26 +85,30 @@ namespace NOU::NOU_MATH
 	}
 
 	template<typename T>
-	Quaternion Quaternion::Normalized()
+	Quaternion Quaternion::normalized()
 	{
 		T length = Length();
 		return  Quaternion(m_x / length, m_y / length, m_z / length, m_w / length);
 	}
 
 	template<typename T>
-	Quaternion Quaternion::Conjugate()
+	Quaternion Quaternion::conjugate()
 	{
 		return  Quaternion(-m_x, -m_y, -m_z, m_w);
 	}
 
 	template<typename T>
-	Quaternion Quaternion::mul(T r)
+	Quaternion Quaternion::mul(const T &r)
 	{
 		return Quaternion(m_x * r, m_y * r, m_z * r, m_w * r);
 	}
 
+	/*
+	\Product of two quaternions
+	*/
+
 	template<typename T>
-	Quaternion Quaternion::mul(Quaternion r)
+	Quaternion Quaternion::mul(const Quaternion &r)
 	{
 		T W = m_w * r.getM_w() - m_x * r.getM_x() - m_y * r.getM_y() - m_z * r.getM_z();
 		T X = m_x * r.getM_w() + m_w * r.getM_x() + m_y * r.getM_z() - m_z * r.getM_y();
@@ -115,7 +119,7 @@ namespace NOU::NOU_MATH
 	}
 
 	template<typename T, sizeType N>
-	Quaternion Quaternion::mul(vector<T, N> r)
+	Quaternion Quaternion::mul(const vector<T, N> &r)
 	{
 		T W = -m_x * r.getM_x() - m_y * r.getM_y() - m_z * r.getM_z();
 		T X = m_w * r.getM_x() + m_y * r.getM_z() - m_z * r.getM_y();
@@ -126,19 +130,19 @@ namespace NOU::NOU_MATH
 	}
 
 
-	Quaternion Quaternion::sub(Quaternion r)
+	Quaternion Quaternion::sub(const Quaternion &r)
 	{
 		return Quaternion(m_x - r.getM_x(), m_y - r.getM_y(), m_z - r.getM_z(), m_w - r.getM_w());
 	}
 
 
-	Quaternion Quaternion::add(Quaternion r)
+	Quaternion Quaternion::add(const Quaternion &r)
 	{
 		return Quaternion(m_x + r.getM_x(), m_y + r.getM_y(), m_z + r.getM_z(), m_w + r.getM_w());
 	}
 
 	template<typename T, sizeType N>
-	Matrix4f Quaternion::ToRotationMatrix()
+	Matrix4f Quaternion::toRotationMatrix()
 	{
 		Vector<T,N> forward = Vector<T, N>(2.0f * (m_x * m_z - m_w * m_y), 2.0f * (m_y * m_z + m_w * m_x), 1.0f - 2.0f * (m_x * m_x + m_y * m_y));
 		Vector<T,N> up = Vector<T, N>(2.0f * (m_x * m_y + m_w * m_z), 1.0f - 2.0f * (m_x * m_x + m_z * m_z), 2.0f * (m_y * m_z - m_w * m_x));
@@ -148,13 +152,13 @@ namespace NOU::NOU_MATH
 	}
 
 	template<typename T>
-	T Quaternion::dot(Quaternion r)
+	T Quaternion::dot(const Quaternion &r)
 	{
 		return m_x * r.getM_x() + m_y * r.getM_y() + m_z * r.getM_z() + m_w * r.getM_w();
 	}
 
 	template<typename T>
-	Quaternion Quaternion::NLerp(Quaternion dest, T lerpFactor, T shortest)
+	Quaternion Quaternion::nLerp(Quaternion dest, T lerpFactor, T shortest)
 	{
 		Quaternion correctedDest = dest;
 
